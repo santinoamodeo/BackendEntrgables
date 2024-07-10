21e335d2b384e5a6419e60a4ef622a90f4edb54f
Santino Amodeo <santinoamodeoph@gmail.com>
2024-07-10 20:26:14 -0300

    soluciion errores

--- 2dapreentrega/src/DAO/models/chat.model.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const schema = new Schema({
-  user: { type: String, required: true, max: 100 },
-  message: { type: String, required: true, max: 300 },
-});
-
-export const ChatModel = model('messages', schema);
--- 2dapreentrega/src/routes/chats.router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-export const chatRouter = express.Router();
-
-chatRouter.get('/', (req, res) => {
-  return res.render('chat', {});
-});
